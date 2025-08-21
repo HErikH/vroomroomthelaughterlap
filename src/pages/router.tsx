173b7b56d0a813112a0bc15e6bfe44bbcda42381
HErikH <erikharutyunyan398@gmail.com>
2025-08-21 22:12:41 +0400

    refactor: refactor router.tsx

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,26 +1,31 @@
 import { createBrowserRouter } from "react-router";
 import { MainLayout } from "./MainLayout";
-import { NotFound } from "./NotFoundPage/NotFoundPage";
+import { NotFoundPage } from "./NotFoundPage/NotFoundPage";
+import { GaragePage } from "./GaragePage/GaragePage";
+import { WinnersPage } from "./WinnersPage/WinnersPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     children: [
-    //   {
-    //     path: "",
-    //     element: <GaragePage />,
-    //   },
-    //   {
-    //     path: "",
-    //     element: <WinnerPage />,
-    //   },
+      {
+        index: true,
+        element: <GaragePage />,
+      },
+      {
+        path: "garage",
+        element: <GaragePage />,
+      },
+      {
+        path: "winners",
+        element: <WinnersPage />,
+      },
       // ! Don't move it to top otherwise even on valid url it will throw not found page
       // ! Because when it on the top other paths ignored "*" matches to any path
       {
         path: "*",
-        element: <NotFound />,
-        // title: "not_found",
+        element: <NotFoundPage />,
       },
     ],
   },
